7b98a87c99347c31d60c29a140de3d895d3055e5
kimwoojin123 <kimwoojin1903@gmail.com>
2023-11-03 17:52:26 +0900

    form으로 제출하는 데이터를 body에 담아서json에 보내려고 함

--- routes.js
+++ routes.js
@@ -6,11 +6,13 @@ router.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-router.post("/", (req, res) => {
+router.post("/data", (req, res) => {
+  const postData = req.body;
+  console.log(req.body);
   const jsonFilePath = "./public/main.json";
   const jsonFile = fs.readFileSync(jsonFilePath);
   const parseJsonFile = JSON.parse(jsonFile);
-  const newObj = {message: "value"};
+  const newObj = [{postData}];
   parseJsonFile.mainContent.inputRecords = newObj;
   fs.writeFileSync(jsonFilePath, JSON.stringify(parseJsonFile));
 });
