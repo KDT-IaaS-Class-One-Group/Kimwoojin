870f218794188489aa83b4303f745db0e22010ee
kimwoojin <kimwoojin1903@gmail.com>
2023-11-05 19:23:08 +0900

    메시지 json에 업데이트 및 화면출력

--- routes.js
+++ routes.js
@@ -7,14 +7,22 @@ router.get("/", (req, res) => {
 });
 
 router.post("/data", (req, res) => {
-  const postData = req.body;
-  console.log(req.body);
+  const postData = req.body.message;
+  console.log(postData);
+
   const jsonFilePath = "./public/main.json";
   const jsonFile = fs.readFileSync(jsonFilePath);
   const parseJsonFile = JSON.parse(jsonFile);
-  const newObj = [{postData}];
-  parseJsonFile.mainContent.inputRecords = newObj;
-  fs.writeFileSync(jsonFilePath, JSON.stringify(parseJsonFile));
+
+  // 기존 데이터를 가져와서 새로운 데이터를 추가합니다.
+  const inputRecords = parseJsonFile.mainContent.inputRecords || [];
+  inputRecords.push({ message: postData });
+  parseJsonFile.mainContent.inputRecords = inputRecords;
+
+  fs.writeFileSync(jsonFilePath, JSON.stringify(parseJsonFile, null, 2));
+
+  // 응답을 보냅니다.
+  res.send({ success: true });
 });
 
 module.exports = router;
