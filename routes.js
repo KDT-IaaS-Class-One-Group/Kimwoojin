06bb4e7e8165de52ad7efed8ac166e6eb0dfa83d
kimwoojin <kimwoojin1903@gmail.com>
2023-11-05 19:35:11 +0900

    햄버거메뉴 코드 변경

--- routes.js
+++ routes.js
@@ -2,6 +2,27 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
+// JSON 파일 초기화
+const jsonFilePath = "./public/main.json";
+const defaultData = {
+  header: {
+    hamburgerMenu: ["Home", "Profile", "Settings", "Logout"],
+    logo: "🌐",
+    promptInputPlaceholder: "Enter your message here...",
+  },
+  mainContent: {
+    inputRecords: [],
+    userInfo: {
+      name: "John Doe",
+      status: "Online",
+      avatar: "👤",
+    },
+  },
+};
+
+fs.writeFileSync(jsonFilePath, JSON.stringify(defaultData, null, 2));
+console.log("JSON 파일 초기화 완료");
+
 router.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
@@ -10,7 +31,6 @@ router.post("/data", (req, res) => {
   const postData = req.body.message;
   console.log(postData);
 
-  const jsonFilePath = "./public/main.json";
   const jsonFile = fs.readFileSync(jsonFilePath);
   const parseJsonFile = JSON.parse(jsonFile);
 
